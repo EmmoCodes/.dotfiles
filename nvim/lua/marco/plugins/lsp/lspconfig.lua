f25f8f88d0ab03fa91c038801faae2a96418066f
Marco Orlandi <Gianluca1996@gmx.de>
2025-07-15 09:48:49 +0200

    cleaned up a bit

--- nvim/lua/marco/plugins/lsp/lspconfig.lua
+++ nvim/lua/marco/plugins/lsp/lspconfig.lua
@@ -30,42 +30,10 @@ return {
 		lspconfig.gopls.setup({
 			on_attach = on_attach,
 			capabilities = capabilities,
-			-- settings = {
-			-- 	gopls = {
-			-- 		["ui.inlayhint.hints"] = {
-			-- 			compositeLiteralFields = false,
-			-- 			constantValues = false,
-			-- 			parameterNames = false,
-			-- 		},
-			-- },
-			-- },
 		})
 		lspconfig.lua_ls.setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
-			-- on_init = function(client)
-			-- 	if client.workspace_folders then
-			-- 		local path = client.workspace_folders[1].name
-			-- 		if
-			-- 			path ~= vim.fn.stdpath("config")
-			-- 			and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc"))
-			-- 		then
-			-- 			return
-			-- 		end
-			-- 	end
-			-- 	client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-			-- 		runtime = {
-			-- 			version = "LuaJIT",
-			-- 		},
-			-- 		workspace = {
-			-- 			checkThirdParty = false,
-			-- 			library = {
-			-- 				vim.env.VIMRUNTIME,
-			-- 				"${3rd}/luv/library",
-			-- 			},
-			-- 		},
-			-- 	})
-			-- end,
 			settings = {
 				Lua = {
 					telemetry = {
